592147422dedfc801fba9d1032bbfdf16f18f5ed
Matthew Holmes <matthew.holmes@mercer.com>
2016-03-29 11:05:10 -0400

    d3.0.2 of evolution-boilerplate-sass

---
+++ patterns/organisms/conversational-form/conversational-form.js
@@ -0,0 +1,17 @@
+//--------------------------------------------------------------------------------------------------------
+// Mercer Evolution v3.0.2
+// Conversational Form v1.0.0 - JS
+// DATE: March 17, 2016
+// AUTHOR: Douglas Fraize, Matthew Holmes, Sherry Seeton
+// URL: http://evolution.mercer.com
+//--------------------------------------------------------------------------------------------------------
+
+// CONVERSATIONAL FORM ITEM TOGGLE  - BEGINS
+	var evoConversationalFormItemToggle = function(){
+		jQuery('[data-evo-conversational-form-element]').focus(function(){
+			var id = 'evo-conversational-form-item-' + jQuery(this).attr('data-evo-conversational-form-item-toggle');
+			jQuery('#' + id).fadeIn(2000);
+		});
+	}
+	evoConversationalFormItemToggle();
+// CONVERSATIONAL FORM ITEM TOGGLE - ENDS
---
+++ patterns/organisms/item-selection-panel/item-selection-panel.js
@@ -0,0 +1,16 @@
+//--------------------------------------------------------------------------------------------------------
+// Mercer Evolution v3.0.2
+// Item Selection Panel v1.0.0 - JS
+// DATE: March 17, 2016
+// AUTHOR: Douglas Fraize, Matthew Holmes, Sherry Seeton
+// URL: http://evolution.mercer.com
+//--------------------------------------------------------------------------------------------------------
+
+// ITEM SELECTION PANEL ITEM SELECTED TOGGLE - BEGINS
+	var evoItemSelectionPanelItemSelected = function (){
+		jQuery('[data-evo-item-selection-panel-item]').change(function() {
+			jQuery(this).toggleClass('evo-item-selection-panel-item-selected');
+		});
+	}
+	evoItemSelectionPanelItemSelected();
+// ITEM SELECTION PANEL ITEM SELECTED TOGGLE - ENDS
